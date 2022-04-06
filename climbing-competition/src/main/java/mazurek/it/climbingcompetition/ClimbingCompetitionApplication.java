c1f75d678e266790274834f6fcf6a4b45b60b938
Mazurek Mateusz <mazzi198913@wp.pl>
2022-04-06 22:00:16 +0200

    hello Stranger

---
+++ climbing-competition/src/main/java/mazurek/it/climbingcompetition/ClimbingCompetitionApplication.java
@@ -0,0 +1,14 @@
+package mazurek.it.climbingcompetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ClimbingCompetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingCompetitionApplication.class, args);
+	}
+
+}
