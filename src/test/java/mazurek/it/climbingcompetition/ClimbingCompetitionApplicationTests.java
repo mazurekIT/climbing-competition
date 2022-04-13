9c05f14d90db8015ec9008dcaac26d7f48d9aa9e
Mazurek Mateusz <mazzi198913@wp.pl>
2022-04-13 22:56:26 +0200

    changed project structure

---
+++ src/test/java/mazurek/it/climbingcompetition/ClimbingCompetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package mazurek.it.climbingcompetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingCompetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
