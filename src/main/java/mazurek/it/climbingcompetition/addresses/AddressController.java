390d41d48498ee6ad3a1bb8a37f66f97d41e8abe
Mazurek Mateusz <mazzi198913@wp.pl>
2022-04-30 14:47:41 +0200

    address controller and service

--- src/main/java/mazurek/it/climbingcompetition/addresses/AddressController.java
+++ src/main/java/mazurek/it/climbingcompetition/addresses/AddressController.java
@@ -1,22 +1,31 @@
 package mazurek.it.climbingcompetition.addresses;
 
 import mazurek.it.climbingcompetition.BasicController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class AddressController extends BasicController {
-    private final AddressRepository addressRepository;
+    private final AddressService addressService;
 
-    public AddressController(AddressRepository addressRepository) {
-        this.addressRepository = addressRepository;
+    @Autowired
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
     }
 
     @PostMapping("/address")
     public Address createAddress(@Valid @RequestBody Address address) {
-        return addressRepository.save(address);
+        return addressService.addAddress(address);
+    }
+
+    @GetMapping("/address")
+    public List<Address> getAll(){
+        return addressService.findAll();
     }
 }
--- src/main/java/mazurek/it/climbingcompetition/addresses/AddressService.java
+++ src/main/java/mazurek/it/climbingcompetition/addresses/AddressService.java
@@ -1,7 +1,24 @@
 package mazurek.it.climbingcompetition.addresses;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AddressService {
+    private final AddressRepository addressRepository;
+
+    @Autowired
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    public Address addAddress(Address address) {
+        return addressRepository.save(address);
+    }
+
+    public List<Address> findAll() {
+        return (List<Address>) addressRepository.findAll();
+    }
 }
