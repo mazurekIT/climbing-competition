1fd767459543f797f3181602a3aeff3126cd89d6
Mazurek Mateusz <mazzi198913@wp.pl>
2022-05-09 22:14:22 +0200

    refactor hello controller and add demo view

--- src/main/java/mazurek/it/climbingcompetition/hellos/HelloController.java
+++ src/main/java/mazurek/it/climbingcompetition/hellos/HelloController.java
@@ -1,14 +1,19 @@
 package mazurek.it.climbingcompetition.hellos;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "name", defaultValue = "Stranger") String name) {
         return "Hello " + name;
+    }
+
+    @GetMapping(path = "/demo")
+    public String demo(){
+        return "demo_ file";
     }
 }
