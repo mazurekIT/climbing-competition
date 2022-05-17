c85c469fd2ba44182adce74304c5902d15c0627b
Mazurek Mateusz <mazzi198913@wp.pl>
2022-05-17 21:53:33 +0200

    add gender controller

---
+++ src/main/java/mazurek/it/climbingcompetition/controller/GenderController.java
@@ -0,0 +1,22 @@
+package mazurek.it.climbingcompetition.controller;
+
+import lombok.RequiredArgsConstructor;
+import mazurek.it.climbingcompetition.model.enums.Gender;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/v1/genders")
+@RequiredArgsConstructor
+public class GenderController {
+
+    @GetMapping()
+    public List<String> getGender() {
+        return Arrays.stream(Gender.values()).map(Gender::getFullName).collect(Collectors.toList());
+    }
+}
