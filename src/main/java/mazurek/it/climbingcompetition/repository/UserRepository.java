c33572eb074e0855713934e06d0df9c9140cc902
Mazurek Mateusz <mazzi198913@wp.pl>
2022-05-22 22:48:17 +0200

    add checking user adding

--- src/main/java/mazurek/it/climbingcompetition/repository/UserRepository.java
+++ src/main/java/mazurek/it/climbingcompetition/repository/UserRepository.java
@@ -2,10 +2,15 @@ package mazurek.it.climbingcompetition.repository;
 
 import mazurek.it.climbingcompetition.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
 
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByMail(String mail);
+
+    Optional<User> findUserByPhone(String phone);
 
 }
--- src/main/java/mazurek/it/climbingcompetition/service/UserService.java
+++ src/main/java/mazurek/it/climbingcompetition/service/UserService.java
@@ -1,6 +1,8 @@
 package mazurek.it.climbingcompetition.service;
 
 import lombok.RequiredArgsConstructor;
+import mazurek.it.climbingcompetition.exceptions.MailDuplicationException;
+import mazurek.it.climbingcompetition.exceptions.PhoneDuplicatedException;
 import mazurek.it.climbingcompetition.exceptions.UserNotFoundException;
 import mazurek.it.climbingcompetition.model.User;
 import mazurek.it.climbingcompetition.repository.UserRepository;
@@ -14,7 +16,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     public List<User> findAll() {
-        return (List<User>) userRepository.findAll();
+        return userRepository.findAll();
     }
 
     public User findUserById(Long id) {
@@ -22,10 +24,9 @@ public class UserService {
     }
 
     public User addUser(User user) {
+        if (userRepository.findUserByMail(user.getMail()).isPresent()) throw new MailDuplicationException();
+        if (userRepository.findUserByPhone(user.getPhone()).isPresent()) throw new PhoneDuplicatedException();
         return userRepository.save(user);
     }
 
-    public List<User> addListOfUsers(List<User> users) {
-        return (List<User>) userRepository.saveAll(users);
-    }
 }
