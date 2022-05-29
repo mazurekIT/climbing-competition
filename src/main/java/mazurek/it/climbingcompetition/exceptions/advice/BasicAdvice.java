2fcf1797fff3b74307629ac81acc30fd4622770c
Mazurek Mateusz <mazzi198913@wp.pl>
2022-05-29 09:15:55 +0200

    basic controller

--- src/main/java/mazurek/it/climbingcompetition/exceptions/advice/BasicAdvice.java
+++ src/main/java/mazurek/it/climbingcompetition/exceptions/advice/BasicAdvice.java
@@ -3,6 +3,7 @@ package mazurek.it.climbingcompetition.exceptions.advice;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,6 +13,7 @@ import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
 
+@ControllerAdvice
 public class BasicAdvice {
 
     @ResponseBody
