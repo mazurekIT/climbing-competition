b221646dabe91545f9ff7f9fa10ae0582288b894
Mazurek Mateusz <mazzi198913@wp.pl>
2022-06-08 22:10:01 +0200

    fix gender names

--- src/main/java/mazurek/it/climbingcompetition/model/enums/Gender.java
+++ src/main/java/mazurek/it/climbingcompetition/model/enums/Gender.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Gender {
-    FEMALE("F", "Mężczyzna"),
-    MALE("M", "Kobieta"),
+    FEMALE("F", "Kobieta"),
+    MALE("M", "Mężczyzna"),
     SENIOR_50("S", "+50"),
     JUNIOR("J", "Junior"),
-    J_MALE("JM", "Junior - dziewczynka"),
-    J_FEMALE("JF", "Junior - chłopiec");
+    J_MALE("JM", "Junior - chłopiec"),
+    J_FEMALE("JF", "Junior - dziewczynka");
     private final String code;
     private final String fullName;
 }
